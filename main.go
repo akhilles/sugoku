7e212c5bc8d552077c3c4d6df26062ce8765b170
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2019-01-21 20:53:33 -0800

    print grid

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 const GridSize = 9
 const MiniGridSize = 3
 const CellStateResolved = 0
@@ -13,4 +17,24 @@ type Cell struct {
 type Grid struct {
 	cells        [GridSize * GridSize]Cell
 	resolveQueue chan int
+}
+
+func (grid *Grid) init() {
+	grid.resolveQueue = make(chan int, GridSize * GridSize)
+	for i := range grid.cells {
+		grid.cells[i].state = CellStateUnresolved
+	}
+}
+
+func (grid *Grid) print() {
+	for i, cell := range grid.cells {
+		if cell.state == CellStateResolved {
+			fmt.Printf("%2v(%9b) ", cell.value, cell.state)
+		} else {
+			fmt.Printf(" -(%9b) ", cell.state)
+		}
+		if (i + 1) % GridSize == 0 {
+			fmt.Println()
+		}
+	}
 }
