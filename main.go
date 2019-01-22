9df805aff174dc42cf046fef9aa98486c18ac43c
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2019-01-21 20:52:45 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+const GridSize = 9
+const MiniGridSize = 3
+const CellStateResolved = 0
+const CellStateUnresolved = 0x1FF
+
+type Cell struct {
+	state uint
+	value int
+}
+
+type Grid struct {
+	cells        [GridSize * GridSize]Cell
+	resolveQueue chan int
+}
