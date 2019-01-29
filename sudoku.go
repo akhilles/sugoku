f8c6d6830d0b5dbd7e90238d255afc91143705d7
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2019-01-28 19:38:41 -0800

    completed solve()

--- sudoku.go
+++ sudoku.go
@@ -5,6 +5,14 @@ import (
 	"math/bits"
 )
 
+type SolveResult int
+
+const (
+	Solved SolveResult = iota
+	NoSolution
+	MultipleSolutions
+)
+
 const GridSize = 9
 const MiniGridSize = 3
 const CellStateResolved uint = 0
@@ -18,14 +26,13 @@ type Cell struct {
 }
 
 type Grid struct {
-	cells        [GridSize * GridSize]Cell
+	cells [GridSize * GridSize]Cell
 	// TODO: switch to fixed size stack (more performant)
 	resolveQueue []int
-	resolved     int
 }
 
 type GridInfo struct {
-	linkedCells [GridSize*GridSize][20]int
+	linkedCells [GridSize * GridSize][20]int
 	groups      [GridSize * 3][]int
 }
 
@@ -45,35 +52,67 @@ func (grid *Grid) processGroups() {
 	for _, group := range gridInfo.groups {
 		oneCell := CellStateResolved
 		multiCell := CellStateResolved
-		for _, cellId := range group {
-			multiCell |= oneCell & grid.cells[cellId].state
-			oneCell |= grid.cells[cellId].state
+		for _, i := range group {
+			multiCell |= oneCell & grid.cells[i].state
+			oneCell |= grid.cells[i].state
 		}
 		oneCell &^= multiCell
-		for _, cellId := range group {
-			if oneCell&grid.cells[cellId].state != 0 && bits.OnesCount(grid.cells[cellId].state) > 1 {
-				grid.cells[cellId].state &= oneCell
-				grid.resolveQueue = append(grid.resolveQueue, cellId)
+		for _, i := range group {
+			if oneCell&grid.cells[i].state != 0 && bits.OnesCount(grid.cells[i].state) > 1 {
+				grid.cells[i].state &= oneCell
+				grid.resolveQueue = append(grid.resolveQueue, i)
 			}
 		}
 	}
 }
 
-func (grid *Grid) solve() bool {
+func (grid *Grid) guessSolve(i int) SolveResult {
+	numSolutions := 0
+	var solutionGrid *Grid
+	for grid.cells[i].state != CellStateResolved {
+		value := bits.TrailingZeros(grid.cells[i].state)
+		fmt.Println(">>> cell", i, "- guessing", value+1)
+
+		grid.cells[i].state &^= 1 << uint(value)
+		newGrid := &Grid{}
+		newGrid.cells = grid.cells
+		newGrid.cells[i].state = 1 << uint(value)
+		newGrid.resolveQueue = []int{i}
+		solveResult := newGrid.solve()
+		if solveResult == Solved {
+			numSolutions++
+			solutionGrid = newGrid
+		}
+		fmt.Println("<<< cell", i, "- guessing", value+1)
+		if solveResult == MultipleSolutions || numSolutions > 1 {
+			return MultipleSolutions
+		}
+	}
+
+	if numSolutions == 1 {
+		grid.cells = solutionGrid.cells
+		return Solved
+	}
+	return NoSolution
+}
+
+func (grid *Grid) solve() SolveResult {
 	// fmt.Println("Q LEN --> ", len(grid.resolveQueue))
 	if len(grid.resolveQueue) == 0 {
-		fmt.Print("GROUP PROCESSING!!! --> ")
+		//fmt.Print("group processing -> ")
 		grid.processGroups()
-		fmt.Println(len(grid.resolveQueue))
+		//fmt.Println(len(grid.resolveQueue))
 	}
 
 	if len(grid.resolveQueue) == 0 {
-		if grid.resolved == GridSize*GridSize {
-			return true
+		// find an unresolved cell
+		for i := range grid.cells {
+			if grid.cells[i].state != CellStateResolved {
+				// make guesses
+				return grid.guessSolve(i)
+			}
 		}
-		numSolutions := 0
-		// start guessing
-		return numSolutions == 1
+		return Solved
 	}
 
 	cellIndex := grid.resolveQueue[0]
@@ -81,15 +120,13 @@ func (grid *Grid) solve() bool {
 	cell := &grid.cells[cellIndex]
 	if cell.state == CellStateResolved {
 		// illegal state
-		println("ILLEGAL STATE")
-		return false
+		return NoSolution
 	}
+
+	// resolve cell
 	value := bits.TrailingZeros(cell.state)
 	cell.value = value
 	cell.state = CellStateResolved
-	grid.resolved++
-
-	// update states of associated linkedCells
 	for _, associatedCell := range gridInfo.linkedCells[cellIndex] {
 		grid.updateCellState(associatedCell, value)
 	}
